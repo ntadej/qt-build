c998016ee4a86f15267730e5b67f97e887bdb688
Tadej Novak <tadej@tano.si>
2024-05-21 22:13:33 +0200

    Static build with Qt 6.7.1

---
+++ static-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+./configure -opensource -confirm-license \
+  -appstore-compliant \
+  -ccache \
+  -prefix /Users/tadej/Workspace/SDK/Source/Qt/6.7.1/macos_static \
+  -release \
+  -static \
+  -no-pch \
+  -nomake tests -nomake examples \
+  -no-sql-mysql -plugin-sql-sqlite \
+  -skip qtactiveqt \
+  -skip qthttpserver \
+  -skip qtmultimedia \
+  -- -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64"
